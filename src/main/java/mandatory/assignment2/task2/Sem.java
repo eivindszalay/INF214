468a655cf3f43963fad4973e66e595c325f7839a
Eivind Szalay <eivind.szalay@student.uib.no>
2021-09-21 10:03:32 +0200

    Started task 2

--- src/main/java/mandatory/assignment2/task2/Sem.java
+++ src/main/java/mandatory/assignment2/task2/Sem.java
@@ -3,21 +3,32 @@ package mandatory.assignment2.task2;
 public class Sem {
 
     private int permits;
+    private int accessLimit;
+
 
     public Sem(int permits) {
+        this.permits = permits;
+    }
+
+    public Sem(int permits, int accesses) {
         this.permits = permits;
     }
 
     public synchronized void acquire() throws InterruptedException {
         if (permits > 0){
             // TODO: Your solution goes here.
+            permits--;
+
         } else {
             // TODO: Your solution goes here.
-        }
+            throw new InterruptedException("The semaphore was not available.");
+            }
     }
+
 
     public synchronized void release() {
         // TODO: Your solution goes here.
+        permits++;
     }
 }
 
